0931dae24b0b72d5b37443a29de793a258727726
Liam G <liamg10@yahoo.co.uk>
2024-07-21 22:32:16 +0000

    feat: add tooltip to foot github link. fix: target -> blank on footer links

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,2 +1,7 @@
+// Enable Popover
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Enable Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
